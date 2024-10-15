68e9722d101205ea5e2dd40bae7d111998b1bc66
reasure3 <reasure3@naver.com>
2024-10-15 14:39:19 +0900

    Custom Tooltips on Items & Blocks

--- src/main/java/com/reasure/neoforge_tutorial/item/ModItems.java
+++ src/main/java/com/reasure/neoforge_tutorial/item/ModItems.java
@@ -2,10 +2,16 @@ package com.reasure.neoforge_tutorial.item;
 
 import com.reasure.neoforge_tutorial.NeoforgeTutorial;
 import com.reasure.neoforge_tutorial.item.custom.ChainsawItem;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(NeoforgeTutorial.MODID);
@@ -15,7 +21,14 @@ public class ModItems {
 
     public static final DeferredItem<Item> CHAINSAW = ITEMS.registerItem("chainsaw", ChainsawItem::new, new Item.Properties().durability(32));
 
-    public static final DeferredItem<Item> TOMATO = ITEMS.registerItem("tomato", Item::new, new Item.Properties().food(ModFoodProperties.TOMATO));
+    public static final DeferredItem<Item> TOMATO = ITEMS.registerItem("tomato", properties -> new Item(properties) {
+        @Override
+        public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag tooltipFlag) {
+            tooltipComponents.add(Component.translatable("tooltip.neoforge_tutorial.tomato.1"));
+
+            super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        }
+    }, new Item.Properties().food(ModFoodProperties.TOMATO));
 
     public static final DeferredItem<Item> FROSTFIRE_ICE = ITEMS.registerItem("frostfire_ice", Item::new, new Item.Properties());
 
--- src/main/java/com/reasure/neoforge_tutorial/item/custom/ChainsawItem.java
+++ src/main/java/com/reasure/neoforge_tutorial/item/custom/ChainsawItem.java
@@ -1,15 +1,20 @@
 package com.reasure.neoforge_tutorial.item.custom;
 
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Objects;
 
 public class ChainsawItem extends Item {
@@ -32,5 +37,17 @@ public class ChainsawItem extends Item {
         }
 
         return InteractionResult.PASS;
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag tooltipFlag) {
+        if (Screen.hasShiftDown()) {
+            tooltipComponents.add(Component.translatable("tooltip.neoforge_tutorial.chainsaw.tooltip.1"));
+            tooltipComponents.add(Component.translatable("tooltip.neoforge_tutorial.chainsaw.tooltip.2"));
+        } else {
+            tooltipComponents.add(Component.translatable("tooltip.neoforge_tutorial.chainsaw.tooltip.shift"));
+        }
+
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
