37488cd46e0320c4211e2043e63f068738df30c2
reasure3 <reasure3@naver.com>
2024-12-05 15:17:22 +0900

    Adding Items to the Composter

--- src/main/java/com/reasure/neoforge_tutorial/datagen/ModDataMapProvider.java
+++ src/main/java/com/reasure/neoforge_tutorial/datagen/ModDataMapProvider.java
@@ -4,6 +4,7 @@ import com.reasure.neoforge_tutorial.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
 import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
 import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 
@@ -18,5 +19,10 @@ public class ModDataMapProvider extends DataMapProvider {
     protected void gather() {
         builder(NeoForgeDataMaps.FURNACE_FUELS)
                 .add(ModItems.FROSTFIRE_ICE, new FurnaceFuel(800), false);
+
+        builder(NeoForgeDataMaps.COMPOSTABLES)
+                .add(ModItems.TOMATO, new Compostable(0.45f, false), false);
+        builder(NeoForgeDataMaps.COMPOSTABLES)
+                .add(ModItems.TOMATO_SEEDS, new Compostable(0.2f, true), false);
     }
 }
